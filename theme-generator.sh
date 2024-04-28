8eaef0deb6f9c68c23490ffcaf58e58b274e0627
Marc <marc.zweiler@outlook.at>
2024-04-29 00:24:48 +0200

    Added ability to specify the txt output

--- theme-generator.sh
+++ theme-generator.sh
@@ -1,20 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "$1" == "" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+if [ "$1" = "" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo "Usage:"
     echo "  theme-generator.sh [IMAGE_PATH] [OPTIONS]"
     echo ""
     echo "Options:"
-    echo "  -s  size        Size to which the image gets compromized before extracting colors."
-    echo "                  The default value is 256. Values should be a power of 2 and ge 64 for best results."
-    echo "  -n  count       number of colors to extract from the image."
-    echo "                  The default value is 5."
     echo "  -f  formats     Output format Option. Default value is g. Can be multiple values"
+    echo "                      p for png"
     echo "                      g for gtk-css"
     echo "                      h for html-css"
-    echo "                      p for png"
-    echo "  -o  output      Output directory. Default directory is $HOME/.config/theme-generator/output"
+    echo "                      t for text-file"
     echo "  -h  --help      Show this help message"
+    echo "  -n  count       number of colors to extract from the image."
+    echo "                  The default value is 5."
+    echo "  -o  output      Output directory. Default directory is $HOME/.config/theme-generator/output"
+    echo "  -s  size        Size to which the image gets compromized before extracting colors."
+    echo "                  The default value is 256. Values should be a power of 2 and ge 64 for best results."
     exit 0
 fi
 
@@ -28,6 +29,7 @@ fi
 res=256
 num=5
 format="g"
+saveTextFile=false
 
 checkOutputDir() {
     if [ -d "$1" ]; then 
@@ -35,7 +37,7 @@ checkOutputDir() {
     else
         echo "Specified output directory does not exist. Do you want to create the directory '$1'? (y/N)"
         read -r answer
-        if [ "$answer" == "y" ] || [ "$answer" == "Y" ]; then
+        if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
             mkdir -p "$1" || echo "Failed to create directory $2" && exit 3
             output="$1"
         else 
@@ -47,7 +49,7 @@ checkOutputDir() {
 
 
 setNextFlag() {
-    if [ "$2" == "" ]; then
+    if [ "$2" = "" ]; then
         echo "Option ${1} requires an argument"
         exit 2
     fi
@@ -74,7 +76,7 @@ fi; if [ "$8" != "" ]; then
     setNextFlag "$8" "$9"
 fi
 
-if [ "$output" == "" ]; then
+if [ "$output" = "" ]; then
     output="$HOME/.config/theme-generator/"
     echo "Using the default output directory: $output"
     checkOutputDir "$output"
@@ -85,15 +87,18 @@ convertAndSaveAsCssForGtk() {
     convert "$srcImage" -geometry "${res}x${res}" -colors "$num" -unique-colors -scale 4000% "/tmp/colors.txt"
     tail -n +2 "/tmp/colors.txt" > "/tmp/colors_trim.txt"
     cut -d ' ' -f 4 "/tmp/colors_trim.txt" > "/tmp/colors_cut.txt" 
-    uniq "/tmp/colors_cut.txt" | head -n "$num" > "${output}colors_hex.txt"
+    uniq "/tmp/colors_cut.txt" | head -n "$num" > "/tmp/colors_hex.txt"
     
     declare -i index=0
-    echo "" > "$output"generated-gtk.css
+    echo "" > /tmp/generated-gtk.css
     while IFS= read -r hexCode; do
         echo "@define-color color${index} ${hexCode};" >> "$output"generated-gtk.css
         index+=1
-    done < <(cat "${output}colors_hex.txt")
+    done < "/tmp/colors_hex.txt"
 
+    if "$saveTextFile"; then
+        cp "/tmp/colors_hex.txt" "$output"colors_hex.txt
+    fi
     rm "/tmp/colors.txt" "/tmp/colors_trim.txt" "/tmp/colors_cut.txt"
 }
 
@@ -102,15 +107,19 @@ convertAndSaveAsCssForHtml() {
     convert "$srcImage" -geometry "${res}x${res}" -colors "$num" -unique-colors -scale 4000% "/tmp/colors.txt"
     tail -n +2 "/tmp/colors.txt" > "/tmp/colors_trim.txt"
     cut -d ' ' -f 4 "/tmp/colors_trim.txt" > "/tmp/colors_cut.txt" 
-    uniq "/tmp/colors_cut.txt" | head -n "$num" > "${output}colors_hex.txt"
+    uniq "/tmp/colors_cut.txt" | head -n "$num" > "/tmp/colors_hex.txt"
 
-    declare -i index=0
-    echo ":root {" > "$output"generated-html.css
+    index=0
+    echo ":root {" > /tmp/generated-html.css
     while IFS= read -r hexCode; do
         printf "\t--color%s: %s;\n" $index "$hexCode" >> "$output"generated-html.css
-        index+=1
-    done < <(cat "${output}colors_hex.txt")
+        index=$((index+=1))
+    done < "/tmp/colors_hex.txt"
     echo "}" >> "$output"generated-html.css
+
+    if "$saveTextFile"; then
+        cp "/tmp/colors_hex.txt" "$output"colors_hex.txt
+    fi
     rm "/tmp/colors.txt" "/tmp/colors_trim.txt" "/tmp/colors_cut.txt"
 }
 
@@ -121,7 +130,17 @@ convertAndSaveAsPNG() {
 
 echo "$format, $res, $num, $output"
 
-if [[ "$format" == *"g"* ]]; then
+# Does only execute one of the three functions, but all of them
+#case "$format" in
+#*g*) convertAndSaveAsCssForGtk "$res" "$num" ;;
+#*h*) convertAndSaveAsCssForHtml "$res" "$num" ;;
+#*p*) convertAndSaveAsPNG "$res" "$num" ;;
+#*) echo "Invalid format" ;;
+#esac
+
+if [[ "$format" == *"t"* ]]; then
+    saveTextFile=true
+fi; if [[ "$format" == *"g"* ]]; then
     convertAndSaveAsCssForGtk "$res" "$num"
 fi; if [[ "$format" == *"h"* ]]; then
     convertAndSaveAsCssForHtml "$res" "$num"
