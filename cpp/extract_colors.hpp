eb78241fa672472491f95d59ac7b9abdceca113c
Marc <marc.zweiler@outlook.at>
2024-05-18 16:25:23 +0200

    Moved type declarations into their own header file

--- cpp/extract_colors.hpp
+++ cpp/extract_colors.hpp
@@ -3,63 +3,8 @@
 #include <vector>
 #include <map>
 
-extern "C" {
-    #define STB_IMAGE_IMPLEMENTATION
-    #include "stb_image.h"
-}
-
-typedef std::map<float, int> HueMap;
-typedef std::tuple<float, int> HueCount;
-
-struct Pixel {
-public:
-    float r;
-    float g;
-    float b;
-    float a;
-
-    Pixel() {
-        r = 0;
-        g = 0;
-        b = 0;
-        a = 0;
-    }
-
-    Pixel(float _r, float _g, float _b, float _a) {
-        r = _r;
-        g = _g;
-        b = _b;
-        a = _a;
-    }
-
-    Pixel operator+(const Pixel& other) const
-    {
-        return Pixel { r + other.r, g + other.g, b + other.b, a + other.a };
-    }
-
-    Pixel operator/(float divisor) const
-    {
-        return Pixel { r / divisor, g / divisor, b / divisor, a / divisor };
-    }
-
-    Pixel operator-(const Pixel& other) const
-    {
-        return Pixel { r - other.r, g - other.g, b - other.b, a - other.a };
-    }
-
-    std::string toString() {
-        return "(" + std::to_string((int)r) + " " + 
-                    std::to_string((int)g) + " " + 
-                    std::to_string((int)b) + " " + 
-                    std::to_string((int)a) + ")";
-    }
-};
-
-struct Image {
-    std::vector<std::vector<Pixel>> data;
-    int width;
-    int height;
-};
+#include "types.hpp"
+#include "sorter.hpp"
 
 /**
  * @brief 
@@ -206,7 +151,7 @@ std::vector<HueCount> getSortedHueCounts(HueMap& map) {
     }
 
     // sort the array
-    
+    sortHueCount(hueCount);
 
     return hueCount;
 }
